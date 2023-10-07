24fbe1ecce6e8c4bbc9d07ac8b9b2c8ad9798ffb
OskarpozPL <118568892+OskarpozPL@users.noreply.github.com>
2023-10-07 15:32:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,22 +6,32 @@ operating_system = platform.system()
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
+black   = "\033[30m"
+red     = "\033[31m"
+green   = "\033[32m"
+yellow  = "\033[33m"
+blue    = "\033[34m"
+magenta = "\033[35m"
+cyan    = "\033[36m"
+white   = "\033[37m"
+reset   = "\033[m"
+
 def clear():
   if operating_system == "Windows":
     os.system("cls")
-  elif operating_system == "Linux":
+  if operating_system == "Linux":
     os.system("clear")
   else:
-    print("[-] We do not support your operating system.")
+    print("{red}[-] We do not support your operating system.{reset}")
     exit()
 
 clear()
 
-print(" ____    _   _   ___   ____    _   _   _____   ____  Copyright (c) 2023.")
-print("|  _ \  | | | | |_ _| / ___|  | | | | | ____| |  _ \ All rights reserved.")
-print("| |_) | | |_| |  | |  \___ \  | |_| | |  _|   | |_) |")
-print("|  __/  |  _  |  | |   ___) | |  _  | | |___  |  _ < ")
-print("|_|     |_| |_| |___| |____/  |_| |_| |_____| |_| \_")
+print(" ____    _   _   ___   ____    _   _   _____   ____   Copyright (c) 2023. ")
+print("|  _ \  | | | | |_ _| / ___|  | | | | | ____| |  _ \  All rights reserved.")
+print("| |_) | | |_| |  | |  \___ \  | |_| | |  _|   | |_) |                     ")
+print("|  __/  |  _  |  | |   ___) | |  _  | | |___  |  _ <                      ")
+print("|_|     |_| |_| |___| |____/  |_| |_| |_____| |_| \_                      ")
 
 print("\n")
 
@@ -29,9 +39,9 @@ print("-------------------------------------------------")
 
 print("\n")
 
-print(f"[*] Operating system: {operating_system}")
-print(f"[*] Hostname: {hostname}")
-print(f"[*] IP Address: {ip_address}")
+print(f"{blue}[*] Operating system: {operating_system}{reset}")
+print(f"{blue}[*] Hostname: {hostname}{reset}")
+print(f"{blue}[*] IP Address: {ip_address}{reset}")
 
 print("\n")
 
