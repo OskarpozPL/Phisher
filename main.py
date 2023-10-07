09756c66600ce5025fe29196a293efd12f61f579
OskarpozPL <118568892+OskarpozPL@users.noreply.github.com>
2023-10-07 15:50:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-import socket
 
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-
-print("--------- DOXBIN ----------")
-print("    Copyright (c) 2023.")
-print("    All rights reserved.")
-print("---------------------------")
-print(f"Hostname: {hostname}")
-print(f"IP Address: {ip_address}")
