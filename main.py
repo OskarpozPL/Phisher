c36d29a1730bcfa6478f4a660d7d11e6be868f0a
OskarpozPL <118568892+OskarpozPL@users.noreply.github.com>
2023-10-07 15:10:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ operating_system = platform.system()
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
+def clear():
+  if operating_system == "Windows":
+    os.system("cls")
+  elif operating_system == "Linux":
+    os.system("clear")
+  else:
+    print("[-] We do not support your operating system.")
+    exit()
+
+clear()
+
 print(" ____    _   _   ___   ____    _   _   _____   ____  ")
 print("|  _ \  | | | | |_ _| / ___|  | | | | | ____| |  _ \ ")
 print("| |_) | | |_| |  | |  \___ \  | |_| | |  _|   | |_) |")
