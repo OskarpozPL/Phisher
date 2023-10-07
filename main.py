db83de787f785cceb1aee94e7c80dc78a2c1e69c
OskarpozPL <118568892+OskarpozPL@users.noreply.github.com>
2023-10-07 15:48:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import os
-import platform
 import socket
 
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
-print("Copyright (c) 2023.")
-print("All rights reserved.")
+print("--------- DOXBIN ----------")
+print("    Copyright (c) 2023.")
+print("    All rights reserved.")
+print("---------------------------")
+print(f"Hostname: {hostname}")
+print(f"IP Address: {ip_address}")
