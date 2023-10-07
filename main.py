a9571f549d089bd64a2ca25f7af7c325f0b67721
OskarpozPL <118568892+OskarpozPL@users.noreply.github.com>
2023-10-07 14:54:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import platform
+import socket
 
+operating_system = platform.system()
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+print(f"Operating system: {operating_system}")
+print(f"Hostname: {hostname}")
+print(f"IP Address: {ip_address}")
