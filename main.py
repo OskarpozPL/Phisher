3950b54fad261433cb02fb84a0e1c24bcbb4ee2e
OskarpozPL <118568892+OskarpozPL@users.noreply.github.com>
2023-10-07 15:03:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ operating_system = platform.system()
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
-print(f"Operating system: {operating_system}")
-print(f"Hostname: {hostname}")
-print(f"IP Address: {ip_address}")
+print(" ____    _   _   ___   ____    _   _   _____   ____  ")
+print("|  _ \  | | | | |_ _| / ___|  | | | | | ____| |  _ \ ")
+print("| |_) | | |_| |  | |  \___ \  | |_| | |  _|   | |_) |")
+print("|  __/  |  _  |  | |   ___) | |  _  | | |___  |  _ < ")
+print("|_|     |_| |_| |___| |____/  |_| |_| |_____| |_| \_")
+
+print("\n")
+
+print("-------------------------------------------------")
+
+print("\n")
+
+print(f"[*] Operating system: {operating_system}")
+print(f"[*] Hostname: {hostname}")
+print(f"[*] IP Address: {ip_address}")
+
+print("\n")
+
+print("-------------------------------------------------")
